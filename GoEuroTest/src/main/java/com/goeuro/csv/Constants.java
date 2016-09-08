645f90d26fc5fa219ace85e481604d16b74f6dc7
Anandh <anandh.nr@gmail.com>
2016-09-08 16:15:35 +0530

    Update Constants.java

--- GoEuroTest/src/main/java/com/goeuro/csv/Constants.java
+++ GoEuroTest/src/main/java/com/goeuro/csv/Constants.java
@@ -9,5 +9,4 @@ public interface Constants {
     public static final String COMMA_DELIMITER = ",";
     public static final String NEW_LINE_SEPARATOR = "\n";
     public static final String FILE_HEADER = "id,name,type,latitude,longitude";
-
 }
