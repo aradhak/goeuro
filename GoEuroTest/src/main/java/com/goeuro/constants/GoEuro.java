f81168d538a36d44f439ea7cde8efd7069d11908
Anandh <anandh.nr@gmail.com>
2016-09-08 16:15:13 +0530

    Update GoEuro.java

--- GoEuroTest/src/main/java/com/goeuro/constants/GoEuro.java
+++ GoEuroTest/src/main/java/com/goeuro/constants/GoEuro.java
@@ -10,4 +10,5 @@ public interface GoEuro {
 	public static final String PROPERTY_FILE="GoEuro";
 	public static final String API_ENDPOINT="CITY_WS_JSON_URL";
 	public static final String CSV_FILE_NAME="CSV_FILE_NAME";
+	
 }
